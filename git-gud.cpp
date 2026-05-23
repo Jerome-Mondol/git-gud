f6e65e3e53583cfd5251bd8977d9254e882c04c8
Jerome Mondol <jeromemondol11@gmail.com>
2026-05-23 22:12:13 +0600

    feat: added constants for terminal color

--- git-gud.cpp
+++ git-gud.cpp
@@ -2,6 +2,15 @@
 #include <string>
 #include <vector>
 
+
+const std::string RESET   = "\033[0m";
+const std::string GREEN   = "\033[32m";
+const std::string RED     = "\033[31m";
+const std::string CYAN    = "\033[36m";
+const std::string BOLD    = "\033[1m";
+
+
+
 int main() {
     return 0;
 }
